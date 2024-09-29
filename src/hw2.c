6c2cf432d9baee9cc81ff25e0ec08d4c0cf097e4
Andrew Shi <164560703+andrewshi2027@users.noreply.github.com>
2024-09-29 17:29:00 +0000

    9/29 1:28PM

--- src/hw2.c
+++ src/hw2.c
@@ -5,8 +5,42 @@
 
 #include "hw2.h"
 
+void int_to_bits (int num) {
+    static char bits[33];
+    bits[32] = '\0';
+    
+    for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        bits[31- i] = bit ? '1' : '0';
+    }
+    //printf("%s", bits);
+    return bits;
+}
+
 void print_packet(unsigned int packet[])
 {
+
+    char* packet_type;
+    int packet_address;
+    int packet_ID;
+    int packet_tag;
+    int packet_Last_BE;
+    int packet_First_BE;
+    int packet_data;    
+
+    //Determine if it's a Write Request or Read Request
+    //Check bits 31 to 10
+    char* 
+
+
+    printf("Packet Type: %d\n", packet_type);
+    printf("Address: %d\n", packet_address);
+    printf("Requester ID: %d\n", packet_ID);
+    printf("Tag: %d\n", packet_tag);
+    printf("Last BE: %d\n", packet_Last_BE);
+    printf("1st BE: %d\n", packet_First_BE);
+    printf("Data: %d\n", packet_data);
+
     (void) packet;
 }
 
--- src/hw2_main.c
+++ src/hw2_main.c
@@ -3,7 +3,11 @@
 #include <assert.h>
 #include "hw2.h"
 
+void int_to_bits();
+
 int main() {
 	//Write your code here
+    int packet = 10;
+    int_to_bits(packet);
     return 0;
 }
